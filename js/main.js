82fb50cd1c1449d988f305eadcbeda83dda95ae4
Beau Harrison <beau@fnal.gov>
2016-09-20 00:32:12 -0500

    Fix data structure. Handle undefined object.

--- js/main.js
+++ js/main.js
@@ -4,11 +4,16 @@ $(document).ready(function(){
 })
 
 function print(obj,info) {
+    if (typeof(obj.data) === 'undefined') {
+        console.log("ERROR: data undefined")
+        return false
+    }
+
     if ($('#container').children().length >= 10) { // 10 readings on page already
         $('#container div:first-child').remove() // remove first reading
     }
 
-    $('#container').append(`<div>${obj.data} @ ${obj.time}</div>`) // append new reading
+    $('#container').append(`<div>${obj.data} @ ${obj.timestamp}</div>`) // append new reading
 }
 
 function liveGet() {
