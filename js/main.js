1becd3b3d55ed135c160debca6c0a977ca7240b5
Beau Harrison <beau@fnal.gov>
2016-09-20 00:43:42 -0500

    Convert time properly

--- js/main.js
+++ js/main.js
@@ -9,11 +9,14 @@ function print(obj,info) {
         return false
     }
 
+    let dateTime = new Date(1000*obj.timestamp),
+        time = timeFromDate(dateTime)
+
     if ($('#container').children().length >= 10) { // 10 readings on page already
         $('#container div:first-child').remove() // remove first reading
     }
 
-    $('#container').append(`<div>${obj.data} @ ${obj.timestamp}</div>`) // append new reading
+    $('#container').append(`<div>${obj.data} @ ${time}</div>`) // append new reading
 }
 
 function liveGet() {
@@ -22,4 +25,12 @@ function liveGet() {
     dpm.addRequest("F:MT6SC1@E,36",print)
 
     dpm.start()
+}
+
+function timeFromDate(date) {
+    let hour = date.getHours(),
+        min = date.getMinutes(),
+        sec = date.getSeconds()
+
+    return hour+':'+min+':'+sec
 }
